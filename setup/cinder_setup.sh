2e43815300c6266b280a9b8c634c3b0e4181872f
Jens-Christian Fischer <jens-christian.fischer@switch.ch>
2013-09-05 11:33:11 +0200

    use single quotes so that $(tenant_id) is not interpreted by shell

--- setup/cinder_setup.sh
+++ setup/cinder_setup.sh
@@ -14,5 +14,5 @@ keystone user-role-add --tenant service --user cinder --role admin
 
 SERVICE_ID_VOLUME=$(keystone service-create --name cinder --type volume --description 'OpenStack Volume Service' | grep ' id ' | get_field 2)
 
-keystone endpoint-create --region RegionOne --service_id $SERVICE_ID_VOLUME --publicurl "http://volume-node.example.org:8776/v1/$(tenant_id)s" --adminurl "http://volume-node.example.org:8776/v1/$(tenant_id)s" --internalurl "http://volume-node:8776/v1/$(tenant_id)s"
+keystone endpoint-create --region RegionOne --service_id $SERVICE_ID_VOLUME --publicurl 'http://volume-node.example.org:8776/v1/$(tenant_id)s' --adminurl 'http://volume-node.example.org:8776/v1/$(tenant_id)s' --internalurl 'http://volume-node:8776/v1/$(tenant_id)s'
 
